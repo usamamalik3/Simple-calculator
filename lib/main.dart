000a102cdb6803faecc3ff66a5c8f763b5dbe871
usama malik <usama0834@gmail.com>
2021-10-23 22:49:50 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Cal());
+}
+
+class Cal extends StatelessWidget {
+  const Cal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Calculator(),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
